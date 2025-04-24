dd9e3e548117f70d7eda6e80cbe9081c35b94084
Revuelt0 <jan@kunc-dc.cz>
2025-04-24 17:34:07 +0200

    Ternánrí operace – Jak zrychlit IF | ELSE IF | ELSE

---
+++ TernaryOperations/TernaryOperations/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+static int s_Level = 15;
+static int s_Speed = 2;
+
+int main()
+{
+	if (s_Level > 5)
+		s_Speed = 10;
+	else
+		s_Speed = 5;
+
+	s_Speed = s_Level > 5 ? 10 : 5; //toto je vpodstatì stejné jako kód nad tímto + je to rychlejší než klasický if else
+
+	std::cout << s_Speed << std::endl;
+
+	std::cin.get();
+}
