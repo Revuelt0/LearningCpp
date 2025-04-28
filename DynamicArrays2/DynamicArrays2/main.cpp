0ef50920052112bd3aa7f4b87748728df1040cc8
Revuelt0 <jan@kunc-dc.cz>
2025-04-28 15:43:15 +0200

    Optimizing Dynamic Arrays

--- DynamicArrays2/DynamicArrays2/main.cpp
+++ DynamicArrays2/DynamicArrays2/main.cpp
@@ -23,9 +23,9 @@ int main()
 {
 	std::vector<Vertex> verticies;
 	verticies.reserve(3);//make enough memory for 3 verticies so you don't have to copy and resize yourself twice
-	verticies.emplace_back(1, 2, 3);
-	verticies.emplace_back(4, 5, 6);
-	verticies.emplace_back(7, 8, 9 );
+	verticies.emplace_back(1, 2, 3);//instead of passing the vertex object like (Vertex(1, 2, 3)) we just passed the parameter list for the constructor
+	verticies.emplace_back(4, 5, 6);//which basically tells the vector cunstruct the vertex objects with the following parameters in out actual vector memory
+	verticies.emplace_back(7, 8, 9 );//that is using emplace_back instead of push_back
 
 	std::cin.get();
 }
