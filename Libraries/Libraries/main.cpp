4d7c5f4be47670fe384b0c07eac9ea2825deb4d4
Revuelt0 <jan@kunc-dc.cz>
2025-04-29 11:09:41 +0200

    Fixed-Libaries

--- Libraries/Libraries/main.cpp
+++ Libraries/Libraries/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
-#include "GLFW/glfw3.h"
 
+extern "C" int glfwInit();
 
 int main()
 {
 	int a = glfwInit();
 
+	std::cout << a << std::endl;
 	std::cin.get();
 }
